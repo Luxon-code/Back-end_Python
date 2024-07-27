121710e838389c28a208205cd55c102f35ca2dcc
Joseph Trujillo <jt9906157@gmail.com>
2024-07-27 12:29:05 -0500

    POST

--- Fast-api/users.py
+++ Fast-api/users.py
@@ -6,15 +6,45 @@ app = FastAPI()
 
 #entidad user
 class User(BaseModel):
+    id:int
     name:str
     surname:str
     age:int
     url:str
     
-users_list = [User(name="joseph",surname="trujillo",age=19,url="no tiene"),
-         User(name="jose",surname="tovar",age=13,url="no tiene"),
-         User(name="david",surname="aguilar",age=19,url="no tiene")]
+users_list = [User(id=1,name="joseph",surname="trujillo",age=19,url="no tiene"),
+         User(id=2,name="jose",surname="tovar",age=13,url="no tiene"),
+         User(id=3,name="david",surname="aguilar",age=19,url="no tiene")]
+
+#get
 
 @app.get("/users")
 async def users():
     return users_list
+
+@app.get("/user/{id}")
+async def user(id:int):
+    return search_user(id)
+    
+    
+@app.get("/user/")
+async def user(id_user:int):
+    return search_user(id_user)
+    
+def search_user(id:int):
+    users = filter(lambda user:user.id==id,users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "no se ha encontrado al usuario"}
+    
+#POST 
+    
+@app.post("/user/")
+async def user(user:User):
+    if type(search_user(user.id)) == User:
+        return {"error": "el usuario ya existe"}
+    else:
+        users_list.append(user)
+        return {"mensaje":"usuario agregado correctamente"}
+
